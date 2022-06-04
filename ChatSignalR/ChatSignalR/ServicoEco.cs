d6839f5b4585a4c94c20eb378b9bfed708fdbc2d
GulhermeC <74925281+GulhermeC@users.noreply.github.com>
2022-06-04 17:54:44 +0100

    chat SignalR versao 1

---
+++ ChatSignalR/ChatSignalR/ServicoEco.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ChatSignalR
+{
+    public class ServicoEco : PersistentConnection
+    {
+        private static Int32 _totalLigacoes = 0;
+
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            Interlocked.Increment(ref _totalLigacoes);
+            Debug.WriteLine("Ligacoes: " + _totalLigacoes);
+            
+            return Connection.Broadcast(string.Format("{0} conectou-se ao serviço eco." + " estão " + ServicoEco._totalLigacoes + " pessoas neste chat", connectionId))
+                .ContinueWith(_ => Connection.Send(connectionId, "Ola, " + connectionId + " tem "+ServicoEco._totalLigacoes+ " pessoas neste chat"));
+        }
+
+        protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled)
+        {
+            Interlocked.Decrement(ref _totalLigacoes);
+            Debug.WriteLine("Ligacoes: " + _totalLigacoes);
+
+            return Connection.Broadcast(string.Format("{0} desconectou-se do serviço eco.", connectionId));
+        }
+
+        protected override async Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            //reenvia a mensagem para todos os clientes menos para o que enviou
+            await Connection.Broadcast(connectionId+":"+data,connectionId);
+            await base.OnReceived(request, connectionId, data);
+        }
+    }
+}
